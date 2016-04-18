e9277d0db574b312effb3bc5b78d77ff5931dfef
Augustin Delporte <delporteaugustin@gmail.com>
2016-04-18 17:53:12 +0000

    "Completed the first program"

---
+++ hello-world.js
@@ -0,0 +1,10 @@
+//First program
+
+function displayHello () {
+    console.log("Hello world!");
+    setTimeout(function() {
+        console.log("Hello world again!");
+    }, 10000);
+}
+
+displayHello();
